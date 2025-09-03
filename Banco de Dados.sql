c440e99a8c357a0006021322c4ddd18e00e8d31d
Awiwa <murilodebiazi@gmail.com>
2025-09-02 22:18:52 -0300

    Update Banco de Dados.sql

--- Banco de Dados.sql
+++ Banco de Dados.sql
@@ -77,26 +77,6 @@ CREATE TABLE itemPedido
  FOREIGN KEY(idProduto) REFERENCES Produto (idProduto)
 ); 
 
-INSERT INTO Cliente VALUES (null,"Maico","Cidade","666","machosigma","99999999999","deusdonovomundo@gmail.com","maico");
-INSERT INTO Cliente VALUES (null,"Murilo","Jangadas","123","nãogay","maisque2","comedordeplastico@gmail.com","maico1");
-INSERT INTO Cliente VALUES (null,"Kesler","Havan","69","naobinario","058058058058","keslerreidelas69@gmail.com","maico2");
-
-INSERT INTO Entregador VALUES (null,"ocaiM","APÉ","6666","deusdoantigomundo@gmail.com","fudido",2,35,false,"maico3");
-INSERT INTO Entregador VALUES (null,"Osterkamp","Bugatti","777","alemãohitler@gmail.com","alemão",3,14,true,"maico4");
-INSERT INTO Entregador VALUES (null,"Lesker","bike","1","meextorquiram@gmail.com","explorado",2.99,19.5,true,"maico5");
-
-INSERT INTO Restaurante VALUES (null,"9","Morte Lenta",4.69,"Hoje","303377777","maico6");
-INSERT INTO Restaurante VALUES (null,"22","Kabum",2.5,"Amanhã","80808080","maico7");
-INSERT INTO Restaurante VALUES (null,"99999","Xis do Mirassol",5,"Amazonia","51","maico8");
-
-INSERT INTO Produto VALUES (null,65535,80,"desgraçamaldita","Não é bom","desgraça",1);
-INSERT INTO Produto VALUES (null,65534,81,"desgraçamaldita2","Não é bom também","desgraça2",1);
-INSERT INTO Produto VALUES (null,0,1,"canudo de plastico","comida do if","plasticos",3);
-INSERT INTO Produto VALUES (null,15,200,"gabinete","da pa comer se tu tentar","gamer",2);
-
-INSERT INTO Pedido VALUES (null,10,false,"1999-10-02 3:33:33",2,null,1,2,3);
-INSERT INTO Pedido VALUES (null,2,true,"2025-09-02 21:17:02",1,"2035-09-02 21:16:03",2,1,1);
-
 CREATE VIEW Cardapio AS 
 SELECT R.nomeRestaurante, P.nomeProduto, P.preco
 FROM Produto P
