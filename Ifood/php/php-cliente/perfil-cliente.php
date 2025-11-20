6882ada0a1f38125a88ff85cfe273dedeb865789
murilodebiazi@gmail.com <murilodebiazi@gmail.com>
2025-11-20 17:19:18 -0300

    Concertando entregador dnv

---
+++ Ifood/php/php-cliente/perfil-cliente.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+include("logar-cliente.php");
+require_once('verificar-sessao-cliente.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="icon" href="../../img/Icon.png" type="image/png">
+  <title>Ifood</title>
+  <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+<body>
+
+  <?php
+  $email = $_SESSION['emailCliente'];
+  $sql = "SELECT * FROM Cliente WHERE emailCliente = '$email'";
+  $resultado = $conexao->query($sql);
+  $linha = $resultado->fetch_assoc();
+  ?>
+
+  <div class="cabecalho">
+    <a id="voltar" href="perfil-cliente.php"><?php echo $linha['nomeCliente'] ?></a>
+    <a id="logo" href="../../html/menu-principal.html"><img src="../../img/Logo.png" alt="Logo"></a>
+    <a id="logout" href="deslogar-cliente.php">Logout</a>
+  </div>
+
+  <div class="corpo">
+    <form class="perfil" action="editar-cliente.php" method="POST">
+      <h1> Perfil </h1>
+      <label>Nome:</label>
+      <input type="text" name="cliente" size="10" value='<?php echo $linha['nomeCliente'] ?>' required>
+
+      <label>CPF:</label>
+      <input type="text" name="cpf" value='<?php echo $linha['CPFCliente'] ?>' required>
+
+      <label>Telefone:</label>
+      <input type="number" name="telefone" value='<?php echo $linha['telefoneCliente'] ?>' required>
+
+      <label>Email:</label>
+      <input type="text" name="email" value='<?php echo $linha['emailCliente'] ?>' required>
+
+      <label>Senha:</label>
+      <input type="password" name="senha" value='<?php echo $linha['senhaCliente'] ?>' required>
+
+      <input class="botao" type="submit" value="Editar">
+
+      <a class="botao-excluir" href="excluir-cliente.php">Excluir Perfil</a>
+    </form>
+  </div>
+
+  <div class="rodape">
+    <p class="copyright">IFood @ 2025 - Murilo, Kesler, Maico, Richard</p>
+  </div>
+</body>
+
+</html>
---
+++ Ifood/php/php-cliente/ver-cardapio.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+include("logar-cliente.php");
+require_once('verificar-sessao-cliente.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="../../img/Icon.png" type="image/png">
+    <title>Ifood</title>
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+<body>
+    <?php
+    $idRestaurante= $_GET['id'];
+    $sqlRestaurante= "SELECT * FROM Restaurante WHERE idRestaurante = '$idRestaurante'";
+    $restaurante= $conexao->query($sqlRestaurante);
+    $linhaR = mysqli_fetch_assoc($restaurante); 
+
+    $sqlProdutos = "SELECT * FROM Produto WHERE idRestaurante = '$idRestaurante'";
+    $produtosListados = $conexao->query($sqlProdutos);
+    ?>
+    <div class="cabecalho">
+        <a id="voltar" href="../php-restaurante/sessao-restaurante.php">Voltar</a>
+        <a id="logo" href="../../html/menu-principal.html"><img src="../../img/Logo.png" alt="Logo"></a>
+        <a id="logout" href="../php-restaurante/deslogar-restaurante.php">Logout</a>
+    </div>
+    <div class="corpo">
+
+        <div class="">
+            <h1><?php echo $linhaR['nomeRestaurante'] ?></h1>
+            <p>Categoria: <?php echo $linhaR['categoria'] ?> </p>
+            <p> <?php echo $linhaR['avaliacao'] ?> </p>
+            <p>Endereço: <?php echo $linhaR['enderecoRestaurante'] ?> </p>
+            <p>Telefone: <?php echo $linhaR['telefoneRestaurante'] ?></p>
+        </div>
+
+        <a href="../php-pedido/criar-pedido.php?id=<?php echo $idRestaurante?>"> + Fazer Pedido</a>
+        
+        <h1>Cardapio:</h1>
+        <div class="cardapio">
+            <?php while ($linhaP = mysqli_fetch_assoc($produtosListados)) { ?>
+                <div class="produto">
+                    <p> <?php echo $linhaP['nomeProduto'];?> </p>
+                    <p> R$<?php echo $linhaP['preco'];?> </p>
+                    <p> <?php echo $linhaP['categoria'];?> </p>
+                    <?php if($linhaP['emEstoque'] == 0){?> <p>Indisponível</p> <?php } ?>
+                </div>
+            <?php } ?>
+        </div>
+
+    </div>
+
+    <div class="rodape">
+        <p class="copyright">IFood @ 2025 - Murilo, Kesler, Maico, Richard</p>
+    </div>
+</body>
+
+</html>
